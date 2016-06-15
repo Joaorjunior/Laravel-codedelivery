79b56dc7b02a83c411401ff50e772f4e7db70fb1
Joaorjunior <joaorjunior97@gmail.com>
2016-06-15 11:08:52 -0300

    Criando API de Deliveryman

---
+++ database/seeds/OrderTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use CodeDelivery\Models\Product;
+use CodeDelivery\Models\Order;
+use Illuminate\Database\Seeder;
+use CodeDelivery\Models\OrderItem;
+
+class OrderTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Order::class, 10)->create()->each(function($o) {
+
+            for($i=0; $i<=2; $i++)
+            {
+                $o->items()->save(factory(OrderItem::class)->make([
+                    'product_id'=> rand(1,5),
+                    'qtd'=>2,
+                    'price'=>50
+
+                ]));
+            }
+        });
+    }
+}
